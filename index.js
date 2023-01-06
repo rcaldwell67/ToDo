471111797dd12a09d0a907ac73a02a0d44f16404
Randall Caldwell <rcaldwell67@gmail.com>
2023-01-06 13:12:51 -0600

    Create SportsStats - RRC_01062023

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
 //creating database structure
 
-const db = new Dexie("Todo App");
-db.version(1).stores({ todos: "++id, todo" });
+const db = new Dexie("SportsStats App");
+db.version(1).stores({ stats: "++id, stat" });
 
 const form = document.querySelector("#new-task-form");
 const input = document.querySelector("#new-task-input");
 const list_el = document.querySelector("#tasks");
 
-//add todo
+//add stat
 form.onsubmit = async (event) => {
   event.preventDefault();
-  const todo = input.value;
-  await db.todos.add({ todo });
-  await getTodos();
+  const stat = input.value;
+  await db.stats.add({ stat });
+  await getStats();
   form.reset();
 };
 
-//display todo
-const getTodos = async () => {
-  const allTodos = await db.todos.reverse().toArray();
-  list_el.innerHTML = allTodos
+//display stat
+const getStats = async () => {
+  const allStats = await db.stats.reverse().toArray();
+  list_el.innerHTML = allStats
     .map(
-      (todo) => `
+      (stat) => `
     
     <div class="task">
     <div class="content">
-    <input id="edit" class="text" readonly="readonly" type="text" value= ${todo.todo}>
+    <input id="edit" class="text" readonly="readonly" type="text" value= ${stat.stat}>
     </div>
     <div class="actions">
-    <button class="delete" onclick="deleteTodo(event, ${todo.id})">Delete</button>
+    <button class="delete" onclick="deleteTodo(event, ${stat.id})">Delete</button>
     </div>
     </div>
     `
     )
     .join("");
 };
-window.onload = getTodos;
+window.onload = getStats;
 
-//delete todo
-const deleteTodo = async (event, id) => {
-  await db.todos.delete(id);
-  await getTodos();
+//delete stat
+const deleteStat = async (event, id) => {
+  await db.stats.delete(id);
+  await getStats();
 };
